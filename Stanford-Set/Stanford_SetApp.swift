aedcea7657fe6b346a70dd04c84afdc9ad0c0d7d
Yuri Cunh <yuriscunha@icloud.com>
2022-08-03 23:31:16 -0300

    Initial Commit

---
+++ Stanford-Set/Stanford_SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_SetApp.swift
+//  Stanford-Set
+//
+//  Created by Yuri Cunha on 03/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
